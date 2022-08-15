45bf348bc11b08bb5c182383a97ab083d1c22255
Leonid Krapchatov <hellsing1998nsk@gmail.com>
2022-08-15 16:16:17 +0700

    Increase intervals to prevent error 429

--- js/ajax.js
+++ js/ajax.js
@@ -1,6 +1,21 @@
 const xhrToBuyData = new XMLHttpRequest()
 const xhrToSellData = new XMLHttpRequest()
 
+setInterval(() => { 
+  xhrToSellData.open("GET", "https://garantex.io/api/v2/otc/ads?direction=sell&payment_method=тинь")
+  xhrToSellData.responseType = "json"
+  xhrToSellData.send()
+}, 4500);
+
+setInterval(() => {
+  xhrToBuyData.open("GET", "https://garantex.io/api/v2/otc/ads?direction=buy&payment_method=тинь")
+  xhrToBuyData.responseType = "json"
+  xhrToBuyData.send()
+}, 4500);
+
+xhrToBuyData.onload = () => { responseHandler(xhrToBuyData, document.querySelector("#buy")) }
+xhrToSellData.onload = () => { responseHandler(xhrToSellData, document.querySelector("#sell")) }
+
 function responseHandler(xhr, table) {
   if (xhr.status === 200) {
     let response = xhr.response
@@ -25,32 +40,19 @@ function responseHandler(xhr, table) {
   }
 }
 
-setInterval(() => { 
-  xhrToSellData.open("GET", "https://garantex.io/api/v2/otc/ads?direction=sell&payment_method=тинь")
-  xhrToSellData.responseType = "json"
-  xhrToSellData.send()
-}, 3000);
-
-setInterval(() => {
-  xhrToBuyData.open("GET", "https://garantex.io/api/v2/otc/ads?direction=buy&payment_method=тинь")
-  xhrToBuyData.responseType = "json"
-  xhrToBuyData.send()
-}, 3000);
-
-xhrToBuyData.onload = () => { responseHandler(xhrToBuyData, document.querySelector("#buy")) }
-xhrToSellData.onload = () => { responseHandler(xhrToSellData, document.querySelector("#sell")) }
-
 function formatAmount(amount) {
   let res = ""
 
   amount = amount.split("")
   let numberOfAfterCommaSimbols = amount.length - amount.findIndex(el => el === ".")
-  let digitsNum = 6 + numberOfAfterCommaSimbols
+  let digitsNum = 9 + numberOfAfterCommaSimbols
   amount = amount.join("")
 
   if (amount.length > digitsNum) {
+    res = `${amount.slice(0, amount.length - digitsNum)} ${amount.slice(amount.length - digitsNum, amount.length - digitsNum + 3)} ${amount.slice(amount.length - digitsNum + 3, amount.length - digitsNum + 6)} ${amount.slice(amount.length - digitsNum + 6, amount.length)}`
+  } else if (amount.length > (digitsNum -= 3)) {
     res = `${amount.slice(0, amount.length - digitsNum)} ${amount.slice(amount.length - digitsNum, amount.length - digitsNum + 3)} ${amount.slice(amount.length - digitsNum + 3, amount.length)}`
-  } else if (digitsNum -= 3) {
+  } else if (amount.length > (digitsNum -= 3)) {
     res = `${amount.slice(0, amount.length - digitsNum)} ${amount.slice(amount.length - digitsNum, amount.length)}`
   }
 
